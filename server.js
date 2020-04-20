eed94e2a167cb710335f4cb08cf03bd8e913a456
Norbert Torok <ntorok@grubhub.com>
2020-04-20 11:10:01 +0300

    deploy

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
-        // Static files
         // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
     server.use('/images', express.static(path.join(__dirname, 'images'), {
         maxAge: dev ? '0' : '365d'
